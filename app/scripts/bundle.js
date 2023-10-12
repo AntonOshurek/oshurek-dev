e7fc08d3eeb49110695e6d84cc4207c1892af7c3
Anton Ashurek <antonashurek@192.168.1.4>
2023-10-12 19:42:05 +0200

    add fonts

--- app/scripts/bundle.js
+++ app/scripts/bundle.js
@@ -1,2 +1,27 @@
-(()=>{"use strict";window.addEventListener("DOMContentLoaded",(()=>{console.log("Hello from gulp :)")}))})();
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	// The require scope
+/******/ 	var __webpack_require__ = {};
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+/*!******************************!*\
+  !*** ./src/scripts/index.js ***!
+  \******************************/
+__webpack_require__.r(__webpack_exports__);
+window.addEventListener('DOMContentLoaded', () => {});
+/******/ })()
+;
 //# sourceMappingURL=bundle.js.map
